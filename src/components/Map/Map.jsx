a6bd1da54bc6d6ba674ffcb8d8d599272954386f
vanderson-alves <v.alves123951@gmail.com>
2022-06-26 00:27:11 -0300

    Configurando Map e List

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlined from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
+  const classes = useStyles();
+  /* const isMobile = useMediaQuery('(min-width:600px)');*/
+
+  const coordinates = { lat: 0, lng: 0 };
+
   return (
-    <div>
-      <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyBzNDu0ZyoZlxkw9BQ6zr4YK5bFMYUU4IU' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={() => {}}
+        onChildClick={() => {}}
+      ></GoogleMapReact>
     </div>
   );
 };
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const PlaceDetails = () => {
+const PlaceDetails = ({ place }) => {
   return (
     <div>
-      <h1>PlaceDetails</h1>
+      <h1>{place.name}</h1>
     </div>
   );
 };
